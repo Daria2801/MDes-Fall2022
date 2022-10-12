0669ee8d2ab13faee42924cc1f9518a88832410f
Daria Ivanova <dariart2801@gmail.com>
2022-10-12 16:04:41 -0700

    Created new visual with P5.js

--- week4/sketch.js
+++ week4/sketch.js
@@ -1,40 +1,22 @@
-let gridStep = 50;
-let radius = 20;
-
-let velX,
-  velY = 2;
+function colorValue(x) {
+  return (x + 1) * 100 * Math.random();
+}
 function setup() {
-  createCanvas(800, 600);
+  createCanvas(500, 500);
   colorMode(HSB, width, height, 100);
+  // background(199, 70, 88);
 }
 
-function draw() {
-  background(0);
-  //showGrid();
-  noStroke();
-
-  for (let x = 0; x < width / gridStep; x = x + 1) {
-    for (let y = 0; y < height / gridStep; y++) {
-      console.log(x, y);
-      let posX = x * gridStep + gridStep * 0.5;
-      let posY = y * gridStep + gridStep * 0.5;
-      posX += random(-5, 5);
-      posY += random(-5, 5);
-
-      fill(posX, 500, 60);
-      //console.log(posX)
-      circle(posX, posY, radius * 2);
-    }
+function draw(x) {
+  // fill(215, 64, 255);
+  // rect(x*10, x*10, width, height);
+  for (let x = 0; x < 10; x = x + 1) {
+    fill(colorValue(x), colorValue(x), colorValue(x));
+    rect(x * 50, x * 50, width, height);
   }
 }
 
-function showGrid() {
-  stroke(180);
-  for (let x = 0; x < width; x = x + gridStep) {
-    line(x, 0, x, height);
-  }
-  stroke(180);
-  for (let y = 0; y < height; y += gridStep) {
-    line(0, y, width, y);
-  }
-}
+//  for(let x = 0; x < 10; x = x+1){
+//    draw(x)
+
+//  }
